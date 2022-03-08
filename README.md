18aa4c8d1d8953cacd4bb6e10607e6b1b131f4c0
kohheepeace <kohheepeace@gmail.com>
2022-03-08 20:51:14 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-nwc4sm
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-nwc4sm)
---
+++ script.js
@@ -0,0 +1,32 @@
+// Define alpine store for confirmModal
+document.addEventListener('alpine:init', () => {
+  Alpine.store('confirmModal', {
+    open: false,
+    title: '',
+    message: '',
+    color: 'red',
+    okText: 'OK',
+    onOk() {},
+    onCancel() {},
+  });
+});
+
+window.customConfirm = (props) => {
+  return new Promise((resolve, reject) => {
+    const confirmModal = window.Alpine.store('confirmModal');
+
+    Object.assign(confirmModal, props);
+
+    confirmModal.onOk = () => {
+      confirmModal.open = false;
+      resolve(true);
+    };
+
+    confirmModal.onCancel = () => {
+      confirmModal.open = false;
+      resolve(false);
+    };
+
+    confirmModal.open = true;
+  });
+};
